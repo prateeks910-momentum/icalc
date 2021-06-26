9a05217f195b726912e6cc87ad1d9f401ef1e133
Prateek Srivastava <psrivastava@lohika.com>
2021-06-26 20:34:40 +0530

    Initial Commit

---
+++ icalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  icalc
+//
+//  Created by Prateek Srivastava on 26/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
